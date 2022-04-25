9a957ba472c7d9ee97a0d7deb2b4151ee602986c
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-04-25 14:41:01 -0300

    Component HelloWorld created

---
+++ practica-uno/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { HelloWorld } from "./components/Title/HelloWorld";
+
+function App() {
+  return (
+    <div className="App">
+      {/* punto 1 */}
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
