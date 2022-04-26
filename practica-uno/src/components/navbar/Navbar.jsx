9d12eb7ddc9701bb4b4b411efab82171f115036d
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-04-26 10:29:01 -0300

    Styles Navbar

--- practica-uno/src/components/navbar/Navbar.jsx
+++ practica-uno/src/components/navbar/Navbar.jsx
@@ -1,20 +1,23 @@
 import styles from "./Navbar.module.css";
 export const Navbar = () => {
+  const helloWorld = ()=>{
+    alert("Hello World!");
+  }
   return (
     <header className={styles.container}>
       <h2 className={styles.title}>PRACTICA 1</h2>
       <ul className={styles.containerLink}>
         <li className={styles.li}>
-          <a className={styles.link} href="">Punto 1</a>
+          <p className={styles.link} onClick={helloWorld}>Hello World!</p>
         </li>
         <li className={styles.li}>
-          <a className={styles.link} href="#form">Punto 2</a>
+          <a className={styles.link} href="#form">Form</a>
         </li>
         <li className={styles.li}>
-          <a className={styles.link} href="#info">Punto 3</a>
+          <a className={styles.link} href="#info">Information</a>
         </li>
         <li className={styles.li}>
-          <a  className={styles.link} href="#counter">Punto 4</a>
+          <a  className={styles.link} href="#counter">Counter</a>
         </li >
       </ul>
     </header>
