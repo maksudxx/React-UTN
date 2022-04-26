a9364b1bbaec214c713202785e1f964c1524643e
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-04-26 01:42:08 -0300

    Navbar created

--- practica-uno/src/components/navbar/Navbar.jsx
+++ practica-uno/src/components/navbar/Navbar.jsx
@@ -1,12 +1,22 @@
-export const Navbar = ()=>{
-    return(
-        <header>
-            <ul>
-            <li><a href="">Punto 1</a></li>
-            <li><a href="#form">Punto 2</a></li>
-            <li><a href="#info">Punto 3</a></li>
-            <li><a href="#counter">Punto 4</a></li>
-            </ul>
-        </header>
-    )
-}
+import styles from "./Navbar.module.css";
+export const Navbar = () => {
+  return (
+    <header className={styles.container}>
+      <h2 className={styles.title}>PRACTICA 1</h2>
+      <ul className={styles.containerLink}>
+        <li className={styles.li}>
+          <a className={styles.link} href="">Punto 1</a>
+        </li>
+        <li className={styles.li}>
+          <a className={styles.link} href="#form">Punto 2</a>
+        </li>
+        <li className={styles.li}>
+          <a className={styles.link} href="#info">Punto 3</a>
+        </li>
+        <li className={styles.li}>
+          <a  className={styles.link} href="#counter">Punto 4</a>
+        </li >
+      </ul>
+    </header>
+  );
+};
